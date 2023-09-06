015bacfe1290cc75c6a837401bbe8f0c1205b7a2
김태훈 <je8035@ipartners.co.kr>
2023-09-06 10:09:59 +0900

    netlify 오류로 해결중 * as S 사용이 원인?

--- src/component/common/Footer.jsx
+++ src/component/common/Footer.jsx
@@ -1,17 +1,10 @@
 // Svg styled component
 import styled from 'styled-components';
-// import * as S from "component/styled/common/Common";
+import {TextP} from "component/styled/common/Common";
 import {colors} from 'component/styled/common/Variable';
 
 
 
-const TextP = styled.p`
-  text-align: ${props => props.align || "left"};
-  color: ${props => props.color || colors.black};
-  & + p { 
-    margin-top: 10px;
-  }
-`;
 
 function Footer () {
   const Footer = styled.div`
