598bcd6fe02d5d21d5ee384f0afdf5903ab43adc
김태훈 <je8035@ipartners.co.kr>
2023-09-06 10:19:10 +0900

    파일명 수정 후 기타 주석 원복

--- src/component/styled/common/AllStyled.jsx
+++ src/component/styled/common/AllStyled.jsx
@@ -1,82 +1,83 @@
-import styled from 'styled-components';
-import { colors, font} from './Variable';
+import styled, {css} from 'styled-components';
+import { sizes, colors, font} from './Variable';
 
 // common 
-// const MarginPadding = css`
-//   margin:${props => props.$margin || "0"};
-//   padding:${props => props.$padding || "0"};
-// `
-// // ♣ Base Css
-// export const Blind = styled.span`
-//   position: absolute;
-//   top: -99999px;
-//   left: -9999px;
-//   opacity: 0;
-//   text-indent: -9999px;
-// `;
+const MarginPadding = css`
+  margin:${props => props.$margin || "0"};
+  padding:${props => props.$padding || "0"};
+`
+// ♣ Base Css
+export const Blind = styled.span`
+  position: absolute;
+  top: -99999px;
+  left: -9999px;
+  opacity: 0;
+  text-indent: -9999px;
+`;
 
-// // ♣ ICON
-// export const Icon = styled.i`
-//   display:inline-block;
-//   position:relative;
-//   width: ${props => props.width || "20px"};
-//   height: ${props => props.width || "20px"};
-//   border-radius: ${props => props.$borderRadius || 0};
-//   ${props => props.$bg 
-//     && 
-//     `
-//       background: ${props.$bg};
-//       border:1px solid ${props.$bg};
-//     `
-//   };
-// `;
+// ♣ ICON
+export const Icon = styled.i`
+  display:inline-block;
+  position:relative;
+  width: ${props => props.width || "20px"};
+  height: ${props => props.width || "20px"};
+  border-radius: ${props => props.$borderRadius || 0};
+  ${props => props.$bg 
+    && 
+    `
+      background: ${props.$bg};
+      border:1px solid ${props.$bg};
+    `
+  };
+`;
 
-// // ♣ Tag 
-// export const A = styled.a`
-//   display:inline-block;
-//   color: ${props => props.color || colors.black};
-// `;
+// ♣ Tag 
+export const A = styled.a`
+  display:inline-block;
+  color: ${props => props.color || colors.black};
+`;
 
-// // ♣ btnWrap
-// export const BtnWrap = styled.div`
-//   display: flex;
-//   ${MarginPadding}
-//   flex-direction: ${props => props.$flex || "row"};
-//   gap:${props => props.gap || "10px"};
-// `;
+// ♣ btnWrap
+export const BtnWrap = styled.div`
+  display: flex;
+  ${MarginPadding}
+  flex-direction: ${props => props.$flex || "row"};
+  gap:${props => props.gap || "10px"};
+`;
 
-// // ♣ BOX
+// ♣ BOX
 
-// // inner
-// export const BoxInner = styled.div`
-//   position:relative;
-//   width:100%;
-//   ${props => !props.full && `max-width: ${props.maxWidth || sizes.table+'px'}`};
-//   margin: 0 auto;
-//   padding:${props => props.$padding || "0"};
-// `;
+// inner
+export const BoxInner = styled.div`
+  position:relative;
+  width:100%;
+  ${props => !props.full && `max-width: ${props.maxWidth || sizes.table+'px'}`};
+  margin: 0 auto;
+  padding:${props => props.$padding || "0"};
+`;
 
-// // line 
-// export const BoxLine = styled.div`
-//   margin:${props => props.$margin || "10px 0 0 0"};
-//   padding:${props => props.$padding || "10px 0 0 0"};
-//   ${props => !(props.$top || props.$right || props.$bottom || props.$left) && `border:1px solid ${colors.lineColor};`}
-//   ${props => props.$top && `border-top:1px solid ${colors.lineColor};` }
-//   ${props => props.$right && `border-right:1px solid ${colors.lineColor};` }
-//   ${props => props.$bottom && `border-bottom:1px solid ${colors.lineColor};` }
-//   ${props => props.$left && `border-left:1px solid ${colors.lineColor};` }
-// `;
+// line 
+export const BoxLine = styled.div`
+  margin:${props => props.$margin || "10px 0 0 0"};
+  padding:${props => props.$padding || "10px 0 0 0"};
+  ${props => !(props.$top || props.$right || props.$bottom || props.$left) && `border:1px solid ${colors.lineColor};`}
+  ${props => props.$top && `border-top:1px solid ${colors.lineColor};` }
+  ${props => props.$right && `border-right:1px solid ${colors.lineColor};` }
+  ${props => props.$bottom && `border-bottom:1px solid ${colors.lineColor};` }
+  ${props => props.$left && `border-left:1px solid ${colors.lineColor};` }
+`;
 
-// // ♣ Sns 
-// export const SnsList = styled.div`
-//   display:flex;
-//   justify-content: ${props => props.$align || "flex-start"};
-//   ${MarginPadding}
-//   font-size:0;
-//   gap:${props => props.$gap || 0 };
-// `;
+// ♣ Sns 
+export const SnsList = styled.div`
+  display:flex;
+  justify-content: ${props => props.$align || "flex-start"};
+  ${MarginPadding}
+  font-size:0;
+  gap:${props => props.$gap || 0 };
+`;
 
 export const TextP = styled.p`
+  ${MarginPadding}
   font-size: ${props => props.fontSize || font.size};
   text-align: ${props => props.align || "left"};
   color: ${props => props.color || colors.black};
