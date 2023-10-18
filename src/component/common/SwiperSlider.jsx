04466088f5ba887dfc528575442c2dd7e5729810
김태훈 <je64768035@gmail.com>
2023-10-18 18:12:09 +0900

    swiper slider 가이드 작성중 1차 샘플

--- src/component/common/SwiperSlider.jsx
+++ src/component/common/SwiperSlider.jsx
@@ -1,26 +1,74 @@
+import { colors } from 'component/styled/common/Variable';
+import styled from 'styled-components';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import Swiper core and required modules
-import { Pagination, Navigation } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-function SwiperSlider() {
+
+
+
+const BaseSlide = styled.div`
+  width:100%;
+  height:200px;
+  background:${colors.subTextColor};
+  color:${colors.whiteColor};
+`
+
+function SwiperSlider({swiperData, swiperOpt}) {
+  console.log(swiperOpt)
+  function onSwiper(swiperE){
+    console.log("init Swiper")
+  }
+  function onSlideChange(changeE) {
+    console.log(changeE)
+  }
   return (
     // 샘플
-    <Swiper pagination={{
-      clickable: true,
-    }}
-    navigation={true}
-    modules={[Pagination, Navigation]}
-    className="mySwiper">
-      <SwiperSlide>Slide 1<br />Slide 1Slide 1Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 1<br />Slide 1Slide 1Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 1<br />Slide 1Slide 1Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 1<br />Slide 1Slide 1Slide 1</SwiperSlide>
-    </Swiper>
+    <>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        {...swiperOpt}
+        onSwiper={onSwiper}
+        onSlideChange={onSlideChange}>
+          {
+            swiperData > 0
+            ?
+              <>
+                <SwiperSlide>Slide 1<br />Slide 1Slide 1Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 1<br />Slide 1Slide 1Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 1<br />Slide 1Slide 1Slide 1</SwiperSlide>
+              </>
+            : 
+              <>
+                <SwiperSlide><BaseSlide>Slide 1 Slide 1Slide 1Slide 1</BaseSlide></SwiperSlide>
+                <SwiperSlide><BaseSlide>Slide 1 Slide 1Slide 1Slide 1</BaseSlide></SwiperSlide>
+                <SwiperSlide><BaseSlide>Slide 1 Slide 1Slide 1Slide 1</BaseSlide></SwiperSlide>
+              </>
+          }
+      </Swiper>
+
+      { // 참고용
+        false &&
+        <Swiper pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper">
+          <SwiperSlide>Slide 1<br />Slide 1Slide 1Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 1<br />Slide 1Slide 1Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 1<br />Slide 1Slide 1Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 1<br />Slide 1Slide 1Slide 1</SwiperSlide>
+        </Swiper>
+
+      }
+    </>
   )
 }
 export default SwiperSlider;
--- src/component/pages/guide/details/GuideSwiper.jsx
+++ src/component/pages/guide/details/GuideSwiper.jsx
@@ -1,10 +1,49 @@
 import SwiperSlider from "component/common/SwiperSlider";
+import * as S from "component/styled/common/AllStyled";
+import { colors } from "component/styled/common/Variable";
+import TitleBar from "component/common/TitleBar";
+import SubTitleBar from "component/common/SubTitleBar";
 
 
 function GuideSwiper () {
+  const swiperOpt = {
+    pagination: {
+      clickable: true,
+    },
+    navigation: true
+  }
   return (
     <div>
-      <SwiperSlider></SwiperSlider>
+      <S.BoxWrap className="search">
+        <S.BoxInner $padding="30px 30px 0">
+          <TitleBar>Swiper Slider</TitleBar>
+          <S.listBarUl $marginTop="20px"> 
+            <S.liBar>
+              <S.TextP><S.colorTag $color={colors.color4}>import 경로 : </S.colorTag> "src/component/common/SwiperSlider";</S.TextP>
+            </S.liBar>
+            <S.liBar>
+              <S.TextP><S.colorTag $color={colors.color4}>참고 : </S.colorTag> https://swiperjs.com/</S.TextP>
+            </S.liBar>
+          </S.listBarUl>
+        </S.BoxInner>
+        <S.BoxLine $top $borderWidth="5px" $borderColor={colors.blackColor} $marginTop="30px" $paddingTop="30px" className="">
+          <S.BoxInner $margin="0 auto" $padding="0 30px">
+            <S.Div>
+              <SubTitleBar fontSize="16px">기본 default</SubTitleBar>
+            </S.Div>
+            <S.Div $marginTop="20px">
+              <SwiperSlider swiperOpt={swiperOpt}></SwiperSlider>
+            </S.Div>
+          </S.BoxInner>
+        </S.BoxLine>
+        <S.BoxLine $top $borderWidth="2px"  $marginTop="30px" $paddingTop="30px" className="">
+          <S.BoxInner $margin="0 auto" $padding="0 30px">
+            <SubTitleBar fontSize="16px">설명 참고</SubTitleBar>
+            <SwiperSlider></SwiperSlider>
+          </S.BoxInner>
+        </S.BoxLine>
+      </S.BoxWrap>
+      
     </div>
   )
 }
