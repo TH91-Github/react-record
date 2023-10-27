bf6b360dbb5e60a5d56b1310d3f233704cab5006
김태훈 <je64768035@gmail.com>
2023-10-27 09:28:20 +0900

    Guide scss 걷어내기 및 컴포넌트 작업 - Font 추가 작업  #1

---
+++ src/component/pages/guide/details/GuideFont.jsx
@@ -0,0 +1,69 @@
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import TitleBar from "component/common/TitleBar";
+import SubTitleBar from "component/common/SubTitleBar";
+
+// styled
+import * as SG from "component/pages/guide/styled/GuideStyled";
+import { fonts } from "component/styled/common/Variable";
+
+function GuideFont() {
+  const fontData = {
+    id:1,
+    title: "Font",
+    desc:[
+      "경로 : component/styled/common/Variable.js",
+      "import { breakpoints } from 'component/styled/common/Variable';",
+      "사용 : fonts.fontB"
+    ],
+    lists: newArrChange(fonts)
+  }
+  function newArrChange(paramObj){
+    return Object.entries(paramObj)
+  }
+  function copyBtn(text){
+    alert(text+" 복사가 완료되었습니다.");
+  }
+
+  return(
+    <>
+      <SG.GuideBoxInner>
+        <SG.GuideTitle>
+          <TitleBar>{fontData.title}</TitleBar>
+        </SG.GuideTitle>
+        <SG.GuideCont>
+          <div className="guide__info">
+            <SubTitleBar $fontSize="16px">설명 참고</SubTitleBar>
+            <div className="guide__info__desc">
+              {
+                fontData.desc.map((breakpointDesc,idx) => (
+                  <SG.GuideTxtBar key={idx}>
+                    {breakpointDesc}
+                  </SG.GuideTxtBar>
+                ))
+              }
+            </div>
+            <div className="guide__list">
+              <SG.FontList>
+                {
+                  fontData.lists.map((fontItme, idx) => (
+                    <li key={idx}>
+                      <CopyToClipboard text={fontItme[0]}>
+                        <SG.FontBtn onClick={()=>copyBtn(fontItme[0])}>
+                          <p>{`${fontItme[0]} : ${fontItme[1]}`}</p>
+                        </SG.FontBtn>
+                      </CopyToClipboard>
+                    </li>
+                  ))
+                }
+              </SG.FontList>
+            </div>
+          </div>
+        </SG.GuideCont>
+      </SG.GuideBoxInner>
+    </>
+  )
+}
+
+
+
+export default GuideFont;
