251ff8147f0ba4303c30111f61b13b02963203ad
김태훈 <je64768035@gmail.com>
2023-10-31 09:13:26 +0900

    텍스트 줄바꿈 TextBr - js 추가

--- src/api/common.js
+++ src/api/common.js
@@ -24,3 +24,14 @@ export const ranDom = (_max, name) => { // 랜덤 값 (최대 값, 이름지정_
   return newId;
 }
 
+
+export function TextBr (text) {  // br 태그 삽입
+  if(!text) return;
+  const brTag ="<br />";
+  // 변경 값을 \n 으로 하고 css : white-spase: pre-wrap; 사용 가능
+  const brChange = text.replaceAll(/(<br>|<br\/>|<br \/>|<\/br>|\r|\n)/gi, brTag).split(brTag);
+  const newBr = brChange.filter(item => item.length > 0); // 공백 배열 요소 제거
+  return newBr.reduce((prevText, currentText) => {
+    return <>{prevText} <br />{currentText.trim()}</>
+  })
+}
