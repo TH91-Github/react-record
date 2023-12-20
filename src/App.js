a261a7136adf00e4bf39cda0fff9b846505f1e5f
김태훈 <je64768035@gmail.com>
2023-12-20 17:11:35 +0900

    기존 작업물 보이게 수정

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.scss';
 import 'assets/scss/common.scss';
 import { GlobalStyles } from "assets/styles/GlobalStyles";
 
+// 삭제 예정
+import MainTemplate from "component/pages/MainTemplate";
 
 function App() {
 
@@ -10,9 +12,9 @@ function App() {
     <div className="App">
       <GlobalStyles />
       {/* 리뉴얼 작업 컴포넌트 */}
-      <BlogTemplate />
+      {/* <BlogTemplate /> */}
       {/* 삭제 예정 */}
-      {/* <MainTemplate />  */}
+      <MainTemplate /> 
     </div>
   );
 }
