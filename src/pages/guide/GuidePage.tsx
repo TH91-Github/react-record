dbcf6f516914f8a6b03862ea142eef4803f19a8a
김태훈 <je64768035@gmail.com>
2025-04-07 16:47:08 +0900

    Chore : gudie > 지원하는 최소 사이즈  안내

--- src/pages/guide/GuidePage.tsx
+++ src/pages/guide/GuidePage.tsx
@@ -1,10 +1,29 @@
 import { guideStyle } from "assets/style/guide/guideStyle";
+import { breakpoints } from "assets/style/variables";
 import { SideLayout } from "components/layout/SideLayout";
 import { SideMenu } from "components/pages/guide/SideMenu";
+import { useCallback, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 
 export const GuidePage = () => {
+  
+  // ✅ 최소 사이즈에 대한 안내
+  const handleResize = useCallback(() => {
+    console.log(window.innerWidth)
+    if(window.innerWidth < breakpoints.tablet) {
+      // 팝업 안내
+    }
+  }, []);
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [handleResize]);
+
   return (
     <StyleWrap className="guide">
       {/* side */}
@@ -22,6 +41,7 @@ export const GuidePage = () => {
 
 const StyleWrap = styled.div`
   position:relative;
+  min-width:${breakpoints.tablet};
   .guide-content {
     position:relative;
     width: 100%;
