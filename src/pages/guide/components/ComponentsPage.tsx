92203df6c580691c2de274974a6b559c7b47584d
김태훈 <je64768035@gmail.com>
2025-04-14 17:23:07 +0900

    Docs : 컴포넌트 가이드 관련 주석

--- src/pages/guide/components/ComponentsPage.tsx
+++ src/pages/guide/components/ComponentsPage.tsx
@@ -7,7 +7,23 @@ export const ComponentsPage = () => {
   return (
     <StyleWrap>
       <GuidePageHeading />
-      
+      {/* 컨텐츠 */}
+    
+      {/* 검색 기능을 여기서 */}
+      {/* detail 페이지 :id */}
+      {/* popup, swiper,  */}
+
+      {/* 
+      리스트 -> 디테일
+
+        title:''
+        desc:''
+        스토리북 url
+        code: highlightjs
+        
+        케이스별 보여주기
+        
+      */}
     </StyleWrap>
   )
 }
