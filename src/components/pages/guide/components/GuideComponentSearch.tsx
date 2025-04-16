11530ed5fd753643aeb2b25437e1bb2c658e6c76
김태훈 <je64768035@gmail.com>
2025-04-16 17:24:02 +0900

    Docs  : 잘못된 주석 수정

--- src/components/pages/guide/components/GuideComponentSearch.tsx
+++ src/components/pages/guide/components/GuideComponentSearch.tsx
@@ -17,23 +17,24 @@ export const GuideComponentSearch = <T extends { category: string }>({
       <SearchModule placeholder="컴포넌트를 검색해보세요"/>
     </StyleWrap>
   )
-    {/* 검색 기능을 여기서 */}
-      {/* detail 페이지 :id */}
-      {/* popup, swiper,  */}
-
-      {/* 
-      리스트 -> 디테일
-
-        title:''
-        desc:''
-        스토리북 url
-        code: highlightjs
-        
-        케이스별 보여주기
-        
-      */}
+   
 
 }
+//  {/* 검색 기능을 여기서 */}
+//       {/* detail 페이지 :id */}
+//       {/* popup, swiper,  */}
+
+//       {/* 
+//       리스트 -> 디테일
+
+//         title:''
+//         desc:''
+//         스토리북 url
+//         code: highlightjs
+        
+//         케이스별 보여주기
+        
+//       */}
 const StyleWrap = styled.div`
   display:flex;
   justify-content: space-between;
