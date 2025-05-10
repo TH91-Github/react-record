fdb539e64f730743a6f72b48627b13a49decb8b8
김태훈 <je64768035@gmail.com>
2025-05-10 18:18:28 +0900

    TEST

--- src/pages/guide/components/view/CarouselViewPage.tsx
+++ src/pages/guide/components/view/CarouselViewPage.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const CarouselViewPage = () => {
-  // test
+
   return(
     <StyleWrap className="view-carousel">
       <p>현재 컴포넌트 설명 및 예시 코드 작성 중에 있습니다...🙇‍♂️</p>
