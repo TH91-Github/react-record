18f5e1a28e0e0cd45415ce18d9b9d66dcc4ef389
김태훈 <je64768035@gmail.com>
2025-05-13 14:24:52 +0900

    Docs : modal 안내 페이지 코드 수정

--- src/components/pages/guide/data/componentsData.ts
+++ src/components/pages/guide/data/componentsData.ts
@@ -35,102 +35,103 @@ export const viewCode = {
       title:'사용 예제',
       lang:'typescript',
       code:
-`
-<Modal onClose={handlePopupClick}>
-  <p>Madal Test</p>
-</Modal>
-`,
+    `
+    <Modal onClose={handlePopupClick}>
+      <p>Madal Test</p>
+    </Modal>
+    `,
     },
     {
       id:'code-tsx',
       title:'TSX',
       lang:'typescript',
       code:
-`
-interface ModalPropsType {
-  isDimmed?: boolean; // dimmed on/off EX: 2중 모달 시 
-  isUnder?: boolean, // 2중 모달일 경우 딤드보다 아래로
-  $width?: number,
-  $align?: 'center' | 'left' | 'right';
-  children?:React.ReactNode;
-  onClose: () => void; // ✅ 닫기 필수
-}
+    `
+    interface ModalPropsType {
+      isDimmed?: boolean; // dimmed on/off EX: 2중 모달 시 
+      isUnder?: boolean, // 2중 모달일 경우 딤드보다 아래로
+      $width?: number,
+      $align?: 'center' | 'left' | 'right';
+      children?:React.ReactNode;
+      onClose: () => void; // ✅ 닫기 필수
+    }
 
-export const Modal = ({
-  isDimmed = true,
-  isUnder,
-  $width = 250,
-  $align = 'center',
-  children,
-  onClose
-}:ModalPropsType) => {
-const handleCloseClick = () => {
-  onClose();
-}
-return (
-    createPortal(
-    <span>
-      🔽 간단 요약
-      .modal-wrap
-      .modal-inner > {children}
-      .dimmed
-    </div>
-    document.body
-    )
-  )
-}
-`
+    export const Modal = ({
+      isDimmed = true,
+      isUnder,
+      $width = 250,
+      $align = 'center',
+      children,
+      onClose
+    }:ModalPropsType) => {
+    const handleCloseClick = () => {
+      onClose();
+    }
+    return (
+        createPortal(
+          <div className="modal-wrap">
+            <div className="modal-inner">
+              {children}
+              ...
+            </div>
+            <div className="dimmed"></div>
+          </div>,
+          document.body
+        )
+      )
+    }
+    `
     },
     {
       id:'code-css',
       title:'CSS',
       lang:'css',
       code:
-`
-.modal-wrap{
-  position:fixed;
-  z-index:100;
-  top:0;
-  left:0;
-  width:100svw;
-  height:100svh;
-  text-align: center;
-}
+    `
+    .modal-wrap{
+      position:fixed;
+      z-index:100;
+      top:0;
+      left:0;
+      width:100svw;
+      height:100svh;
+      text-align: center;
+    }
 
-.modal-inner{
-  position:absolute;
-  z-index:102;
-  top:50%;
-  left:50%;
-  width: \${({$width}) => \$width}px;
-  max-width:80%;
-  padding:30px;
-  border-radius:5px;
-  background: #fff;
-  box-shadow: rgba(17,19,32,0.2) 1px 1px 6px;
-  transform: translate(-50%, -50%);
-  .close-btn {
-    top:10px;
-    right:10px;
-  }
-}
+    .modal-inner{
+      position:absolute;
+      z-index:102;
+      top:50%;
+      left:50%;
+      width: \${({$width}) => \$width}px;
+      max-width:80%;
+      padding:30px;
+      border-radius:5px;
+      background: #fff;
+      box-shadow: rgba(17,19,32,0.2) 1px 1px 6px;
+      transform: translate(-50%, -50%);
+      .close-btn {
+        top:10px;
+        right:10px;
+      }
+    }
 
-.modal-wrap.under {
-  .modal-inner{
-    z-index:90;
-  }
-}
+    .modal-wrap.under {
+      .modal-inner{
+        z-index:90;
+      }
+    }
 
-.dimmed {
-  position: absolute;
-  z-index:101;
-  top:0;
-  left:0;
-  width:100%; 
-  height:100%;
-  background: rgba(0,0,0,0.5);
-}
-`
+    .dimmed {
+      position: absolute;
+      z-index:101;
+      top:0;
+      left:0;
+      width:100%; 
+      height:100%;
+      background: rgba(0,0,0,0.5);
+    }
+    `
     }
   ]
 }
